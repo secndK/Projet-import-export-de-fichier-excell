715926c52ed4e2d0ecdca94a50b7f972e4550ab3
secndk <jilmay02@gmail.com>
2024-11-27 10:54:34 +0000

    projet relatif à l'import et l'export de fichier excel

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+use App\Http\Controllers\UserController;
+
+Route::get('users', [UserController::class, 'index']);
+Route::get('users-export', [UserController::class, 'export'])->name('users.export');
+Route::post('users-import', [UserController::class, 'import'])->name('users.import');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
